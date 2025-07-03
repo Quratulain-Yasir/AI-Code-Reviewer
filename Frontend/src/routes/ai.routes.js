1ed6b31e56a20045d6128240c514aeada7793793
Quratulain-Yasir <hiraiqbal681@gmail.com>
2025-07-03 15:10:10 +0500

    Delete Frontend/src/routes directory

--- Frontend/src/routes/ai.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router = express.Router();
-const aiController = require("../controllers/ai.controllers")
-
-router.post("/get-review" , aiController.getReview)
-
-
-module.exports = router;
